76ade70304edfcd045be76140c0648a6a5de9648
bask185 <sebastiaan_knippels4@hotmail.com>
2023-02-13 23:11:07 +0100

    nearly done. I need to save the speeds to the loco's and I need to check so an already added loco is not added again

--- trainStopper.ino
+++ trainStopper.ino
@@ -8,26 +8,53 @@ Debounce masterSwitch( daPin ) ;
 
 XpressNetMasterClass Xnet ;
 
-const int nLocos = 25 ;
+const int nSlots = 25 ;
 struct 
 {
-    uint16_t address ;
-    uint8_t speed ;
-    uint8_t order ;
-} loco[nLocos];
+    uint16_t    address ;
+    uint8_t     speed ;
+} loco[nSlots];
 
 uint8_t currentLoco ;
 
-void notifyXNetLocoDrive128() // keep track of active locos and alocate slots to them.
+void freeSlot( uint8_t address )
 {
+    for( int i = 0 ; i < nSlots ; i ++ )
+    {
+        if( address == loco[i].address )
+        {
+            loco[i].address = 0xFFFF ;
+            return ;
+        }
+    }
+}
 
+void allocateSlot( uint8_t address )
+{
+    for( int i = 0 ; i < nSlots ; i ++ )
+    {
+        if( loco[i].address == 0xFFFF ) loco[i].address = address ;
+        return ;
+    }
+}
+
+void notifyXNetLocoDrive128( uint16_t address, uint8_t speed ) // keep track of active locos and alocate slots to them.
+{
+    if( speed == 0 ) freeSlot( address ) ;
+    else             allocateSlot( address ) ;
 }
 
 
 void setup()
 {
     Xnet.setup( Loco128,  2 ) ;
+
+    for( int i = 0 ; i < nSlots ; i ++ )
+    {
+        loco[i].address = 0xFFFF ;
+    }
 }
+
 void loop()
 {
     REPEAT_MS( 20 )
@@ -51,7 +78,7 @@ void loop()
         {
             uint16_t address = loco[currentLoco].address ;
             Xnet.setSpeed( address, Loco128, 0 ) ;      
-            if( ++ currentLoco == nLocos ) currentLoco = 0 ;
+            if( ++ currentLoco == nSlots ) currentLoco = 0 ;
         }
         END_REPEAT
 
@@ -59,3 +86,9 @@ void loop()
 
     Xnet.update() ;
 }
+
+/* humanifacation
+    if a train is driving, a slot must be allocated
+    if a train is stationary, a slot must be freed
+    we work under the assumption that no more than 25 trains are used (though there is more memory avaiable.
+*/
