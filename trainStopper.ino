9c65c199eef0f581972dbd87d3e54c1c21735e2e
bask185 <sebastiaan_knippels4@hotmail.com>
2023-02-13 22:15:37 +0100

    added code base. Project compiles. Slot system should be implemented

--- trainStopper.ino
+++ trainStopper.ino
@@ -1,15 +1,61 @@
-#include "src/io.h"
-#include "src/date.h"
-#include "src/version.h"
 #include "src/macros.h"
-void setup()
+#include "src/debounceClass.h"
+#include "src/XpressNetMaster.h"
+
+
+const int daPin = 3 ;
+Debounce masterSwitch( daPin ) ;
+
+XpressNetMasterClass Xnet ;
+
+const int nLocos = 25 ;
+struct 
 {
-    initIO() ;
-    Serial.begin( 115200 ) ;
-    Serial.println( version ) ;
-    Serial.println( date ) ;
+    uint16_t address ;
+    uint8_t speed ;
+    uint8_t order ;
+} loco[nLocos];
+
+uint8_t currentLoco ;
+
+void notifyXNetLocoDrive128() // keep track of active locos and alocate slots to them.
+{
+
 }
 
+
+void setup()
+{
+    Xnet.setup( Loco128,  2 ) ;
+}
 void loop()
 {
+    REPEAT_MS( 20 )
+    {
+        masterSwitch.debounce() ;
+    }
+    END_REPEAT
+
+    uint8_t state = masterSwitch.getState() ;
+    if( state == FALLING )
+    {
+        
+    }
+    if( state == RISING )
+    {
+
+    }
+    if( state == LOW )
+    {
+        REPEAT_MS(50)
+        {
+            uint16_t address = loco[currentLoco].address ;
+            Xnet.setSpeed( address, Loco128, 0 ) ;      
+            if( ++ currentLoco == nLocos ) currentLoco = 0 ;
+        }
+        END_REPEAT
+
+    }
+
+    Xnet.update() ;
 }
